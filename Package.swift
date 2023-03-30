28a82a0b1768897d491baf24c53069c356fa299a
Leon.yan <yanyin1986@gmail.com>
2023-03-30 18:45:24 +0900

    Up version 2.0.3

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
         .library(name: "WechatOpenSDK", targets: ["WechatOpenSDK"]),
     ],
     targets: [
-        .binaryTarget(name: "WechatOpenSDK", url: "https://github.com/yanyin1986/WechatOpenSDK/releases/download/2.0.2/WechatOpenSDK.xcframework.zip", checksum: "ecf1f00e5d076dac57482161560b69809a97fc03072943289d07fcd53742d79f")
+        .binaryTarget(name: "WechatOpenSDK", url: "https://github.com/yanyin1986/WechatOpenSDK/releases/download/2.0.3/WechatOpenSDK.xcframework.zip", checksum: "ecf1f00e5d076dac57482161560b69809a97fc03072943289d07fcd53742d79f")
     ]
 )
